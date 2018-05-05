b28ddcf614d85b3b4528c1018fec2d9a2790921c
existy <existy@MacBook-Air>
2018-05-05 12:59:14 +0900

    change karma to jest

--- app/test/main.Spec.ts
+++ app/test/main.Spec.ts
@@ -1,13 +1,18 @@
 import assert from 'power-assert';
 import Main from '../src/main';
 
-// document.body.innerHTML = __html__['../index.html'];
 describe("Main", () => {
     let t = new Main();
-    it("make bitcoin address test (livenet)", () => {
+    it("make bitcoin address test hash_num_1 (livenet)", () => {
         assert.deepEqual(t.text2bitcoin_address("text", 1, "livenet"),
          ["982d9e3eb996f559e633f4d194def3761d909f5a3b647d1a851fead67c32c9d1",
           "1LpNafS8Nw7MnjJyFiafgok4CUBAJNBHqP"]);
+        });
+
+    it("make bitcoin address test hash_num_5 (livenet)", () => {
+        assert.deepEqual(t.text2bitcoin_address("text", 5, "livenet"),
+         ["a50f91eb5f44ea2af184a058c3d7d2d77460eea0dffdfa74be44d0d00ba23e99",
+          "1F3xM9TCpFFVqbTfpyRDqzSoxgPYG9K8ny"]);
         });
 
     /*
