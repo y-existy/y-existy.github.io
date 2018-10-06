dd61af38b0360170185f062aa296e8c177b36301
existy <existy@MacBook-Air>
2018-10-06 23:39:39 +0900

    WIP

--- app/src/main.ts
+++ app/src/main.ts
@@ -10,15 +10,19 @@ import * as $ from 'jquery';
 import * as bitcore from 'bitcore-lib';
 import * as explorers from 'bitcore-explorers';
 import * as jsSHA from 'jssha';
+import * as xmlhttprequest from 'xmlhttprequest';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './css/base.css';
 
 export default class Main {
-    constructor(){
+    // "testnet" | "livenet"
+    public network = "";
+
+    constructor() {
         this._initlisten();
     }
 
-    private _makeKey(target, num){
+    private _makeKey(target, num) {
         // sha256のハッシュ作成
         var sha256 = new jsSHA("SHA-256", "TEXT");
         sha256.update(target);
@@ -27,59 +31,103 @@ export default class Main {
             return hash;
         } else {
             return this._makeKey(hash, num - 1);
-            }
         }
+    }
 
-    private _makeAddress(privkey, network){
+    private _makeAddress(privkey) {
         // 公開鍵作成
-            var privateKey = new bitcore.PrivateKey(privkey, network);
-            var publicKey = privateKey.toPublicKey();
-
+        var privateKey = new bitcore.PrivateKey(privkey, this.network);
+        var publicKey = privateKey.toPublicKey();
         // bitcoin アドレス作成
-            var pubKey = publicKey;
-            var publicKey = new bitcore.PublicKey(pubKey, network);
-            var address = publicKey.toAddress();
-            var str_address = address.toString();
-            return str_address;
-        }
+        var address = (new bitcore.PublicKey(publicKey, this.network)).toAddress();
+        var str_address = address.toString();
+        return str_address;
+    }
 
-    public text2bitcoin_address(text, num, network){
+    private _makeAddress_hash(privkey) {
+        // 公開鍵作成
+        var privateKey = new bitcore.PrivateKey(privkey, this.network);
+        var publicKey = privateKey.toPublicKey();
+        // bitcoin アドレス作成
+        var address = (new bitcore.PublicKey(publicKey, this.network)).toAddress();
+        return address;
+    }
+
+    public text2bitcoin_address(text, num) {
         var key = this._makeKey(text, num);
-        return [key, this._makeAddress(key, network)];
+        return [key, this._makeAddress(key)];
+    }
+    /*
+        bitcoinを送信するところまで作成したい。
+        やり方としては、現アドレスのUTXOがどれくらい残っているか把握するために
+        blockchainAPIを利用。
+        そこで取得したデータからTxを作成して、insightのwebページに
+        無理やりpostするという流れをとる。
+    */
+
+    public get_request(url): Promise<any> {
+        const XMLHttpRequest = xmlhttprequest.XMLHttpRequest;
+        let request = new XMLHttpRequest();
+        request.open('GET', url, false);
+        request.send(null);
+        return request.responseText;
+    }
+
+    public async getUTXOs(address) {
+        let url = ""
+        if (this.network == "testnet") {
+            url = "https://testnet.blockchain.info/ja/unspent?active=" + address;
+        } else if (this.network == "livenet") {
+            url = "https://blockchain.info/ja/unspent?active=" + address;
+        }
+        const result = await this.get_request(url);
+        return result;
+    }
+
+    public async address2utxos(address) {
+        const utxo_json = JSON.parse(await this.getUTXOs(address));
+        let utxos = [];
+        utxo_json["unspent_outputs"].forEach(function (tx) {
+            var utxo = new bitcore.Transaction.UnspentOutput({
+                "txid": tx["tx_hash"],
+                "vout": tx["tx_output_n"],
+                "address": address,
+                "scriptPubKey": tx["script"],
+                "satoshis": tx["value"]
+            });
+            utxos.push(utxo);
+        }
+        );
+        return utxos;
+    }
+
+    public async makeTransaction(from_address, priv_key, amount, to_address) {
+        const utxos = await this.address2utxos(from_address);
+        console.log(utxos)
+        let tx = new bitcore.Transaction()
+        .fee(10000)
+        .from(utxos)
+        .to(to_address, amount)
+        .change(from_address)
+        .sign(priv_key);
+        return tx.serialize();
     }
 
     private _makeQrcode(id, text) {
         jQuery(id).empty();
         jQuery(id).qrcode({
-            render	: "table",
-            text	: text
+            render: "table",
+            text: text
         });
     }
-/*
-    public check_asset(address, network){
-        //var insight = new explorers.Insight("https://test-insight.bitpay.com",network);
-        var insight = new explorers.Insight();
-        console.log(address);
-        insight.getUnspentUtxos(address, function(err, utxos) {
-            console.log("START");
-            if (err) {
-                console.log("ERROR:check_asset");
-                return "ERROR:check_asset"
-            } else {
-                console.log("utxos");
-                console.log(utxos);
-                return utxos;
-            }
-        });
-    }
-*/
-    private _initlisten() :void{
+
+    private _initlisten(): void {
         var me = this;
-        $("#generate").on("click", function(event){
+        $("#generate").on("click", function (event) {
+            me.network = String($('[name=network]').val());
             let key = $(".input_key").val();
             let num = $(".input_num").val();
-            let network = $('[name=network]').val();
-            let [priv_key, address] = me.text2bitcoin_address(key, Number(num), network);
+            let [priv_key, address] = me.text2bitcoin_address(key, Number(num));
             $(".address").val(address);
             me._makeQrcode("#address_qrcode", address);
             $(".priv_key").val(priv_key);
--- app/test/main.Spec.ts
+++ app/test/main.Spec.ts
@@ -4,23 +4,26 @@ import Main from '../src/main';
 describe("Main", () => {
     let t = new Main();
     it("make bitcoin address test hash_num_1 (livenet)", () => {
-        assert.deepEqual(t.text2bitcoin_address("text", 1, "livenet"),
-         ["982d9e3eb996f559e633f4d194def3761d909f5a3b647d1a851fead67c32c9d1",
-          "1LpNafS8Nw7MnjJyFiafgok4CUBAJNBHqP"]);
-        });
+        t.network = "livenet";
+        assert.deepEqual(t.text2bitcoin_address("text", 1),
+            ["982d9e3eb996f559e633f4d194def3761d909f5a3b647d1a851fead67c32c9d1",
+                "1LpNafS8Nw7MnjJyFiafgok4CUBAJNBHqP"]);
+    });
 
     it("make bitcoin address test hash_num_5 (livenet)", () => {
-        assert.deepEqual(t.text2bitcoin_address("text", 5, "livenet"),
-         ["a50f91eb5f44ea2af184a058c3d7d2d77460eea0dffdfa74be44d0d00ba23e99",
-          "1F3xM9TCpFFVqbTfpyRDqzSoxgPYG9K8ny"]);
-        });
+        t.network = "livenet";
+        assert.deepEqual(t.text2bitcoin_address("text", 5),
+            ["a50f91eb5f44ea2af184a058c3d7d2d77460eea0dffdfa74be44d0d00ba23e99",
+                "1F3xM9TCpFFVqbTfpyRDqzSoxgPYG9K8ny"]);
+    });
 
-    /*
-    it("check bitcoin address (testnet)", () => {
-        assert.deepEqual(t.check_asset(
-            "mhabz8AyY8vKAPkDZWjLAAbbUbcKCovhvF","testnet"),
-            undefined);
-        });
-    */
+    it("make transaction", async() => {
+        t.network = "testnet";
+        // yaguchi_coin 517 addres
+        let test_address = "mhabz8AyY8vKAPkDZWjLAAbbUbcKCovhvF";
+        let test_address_key = "083361f9693d54aabfd4997bc5ab22a2d05a5ae24c2b7b8669242d0d52135144";
+        const res = await t.makeTransaction(test_address, test_address_key, 5000, test_address);
+        console.log(res);
+        assert.deepEqual(res,"test");
+    });
 });
-
